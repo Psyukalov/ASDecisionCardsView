5b8373da1282d13ac51e8ce1430d6802280776d0
Psyukalov <psyukalov@gmail.com>
2018-08-02 18:36:03 +0300

    Add files via upload

--- ASDecisionCardsView/ASDecisionCardsView.h
+++ ASDecisionCardsView/ASDecisionCardsView.h
@@ -10,10 +10,24 @@
 #import <UIKit/UIKit.h>
 
 
+/**
+ 
+ @brief Decision type.
+ 
+ */
+
+typedef NS_ENUM(NSUInteger, ASDecisionType) {
+    ASDecisionTypeCancel = 0,
+    ASDecisionTypeUndefined,
+    ASDecisionTypePositive,
+    ASDecisionTypeNegative,
+};
+
+
 @class ASDecisionCardsView;
 
 
-@protocol ASDecisionCardProtocol <NSObject>
+@protocol ASCardProtocol <NSObject>
 
 @optional
 
@@ -21,8 +35,8 @@
  
  Implement this method for custom reaction to changing offset.
  
- @param view ASDecisionCardsView's superview of UIView's object.
- @param offset CGFloat value of horizontal offset from superview's center to UIView's object center. Сan be from -1.f to 1.f (inclusive both).
+ @param view ASDecisionCardsView's superview of card.
+ @param offset CGFloat value of horizontal offset from superview's center to card center. Сan be from -1.f to 1.f (inclusive both).
  
  */
 
@@ -31,16 +45,129 @@
 @end
 
 
+@protocol ASDecisionCardsViewDelegate <NSObject>
+
+@optional
+
+/**
+ 
+ Implement this method for card customize before his moving to visible queue, such as alpha, CGAffineTransform and etc.
+ 
+ @param view ASDecisionCardsView's object.
+ @param card Card for customize from cards queue at index 'visibleCardsCount' or less if this parameter greater than cards count in queue.
+ 
+ */
+
+- (void)decisionCardsView:(ASDecisionCardsView *)view cardBeforeMovingToVisibleQueue:(UIView *)card;
+
+/**
+ 
+ Implement this method for card customize after his moving to visible queue, such as alpha, CGAffineTransform and etc.
+ 
+ @param view ASDecisionCardsView's object.
+ @param card Card for customize from cards queue at index 'visibleCardsCount' - 1 or less if this parameter greater than cards count in queue.
+ 
+ */
+
+- (void)decisionCardsView:(ASDecisionCardsView *)view cardAfterMovingToVisibleQueue:(UIView *)card;
+
+/**
+ 
+ Implement this method for card customize after his moving to top, such as alpha, CGAffineTransform and etc.
+ 
+ @param view ASDecisionCardsView's object.
+ @param card Card for customize from cards queue at index 0.
+ 
+ */
+
+- (void)decisionCardsView:(ASDecisionCardsView *)view cardAfterMovingToTop:(UIView *)card;
+
+/**
+ 
+ Implement this method for define reaction to decision made. If also needed use first card (top card) use ASDecisionCardsView's object property 'topCard'.
+ 
+ @param view ASDecisionCardsView's object.
+ @param type ASDecisionType value of maded decision.
+ 
+ @return BOOL value that says can we skip top card or not. If method is not implemented default value is YES (always skipping) except when type is equal to ASDecisionTypeCancel.
+ 
+ */
+
+- (BOOL)decisionCardsView:(ASDecisionCardsView *)view didMadeDecisionType:(ASDecisionType)type;
+
+@end
+
+
 @interface ASDecisionCardsView : UIView
+
+/**
+ 
+ @property delegate
+ 
+ @brief ASDecisionCardsView's delegate.
+ 
+ */
+
+@property (weak, nonatomic) id<ASDecisionCardsViewDelegate> delegate;
 
 /**
  
  @property visibleCardsCount
  
- @brief Set count of visible card on ASDecisionCardsView's view;
+ @brief Set count of visible cards for ASDecisionCardsView's object. Minimum is 1, maximum is 32. Default is 4.
  
  */
 
 @property (assign, nonatomic) NSUInteger visibleCardsCount;
+
+/**
+ 
+ @property cardsCount
+ 
+ @brief Cards count from cards views queue.
+ 
+ */
+
+@property (assign, nonatomic, readonly) NSUInteger cardsCount;
+
+/**
+ 
+ @property topCard
+ 
+ @brief Card in cards queue at index 0.
+ 
+ */
+
+@property (strong, nonatomic, readonly) UIView *topCard;
+
+/**
+ 
+ This method add new card to cards queue.
+ 
+ @param card Сard that need to add.
+ 
+ */
+
+- (void)addCard:(nonnull UIView *)card;
+
+/**
+ 
+ This method remove all card from cards queue.
+ 
+ @param animated Says that need to remove with animation.
+ 
+ */
+
+- (void)removeCardsAnimated:(BOOL)animated;
+
+/**
+ 
+ This method made decision for top card.
+ 
+ @param type Says that decision type need for top card.
+ 
+ */
+
+- (void)madeDecisionWithType:(ASDecisionType)type;
 
 @end
--- ASDecisionCardsView/ASDecisionCardsView.m
+++ ASDecisionCardsView/ASDecisionCardsView.m
@@ -11,17 +11,132 @@
 
 
 #define DEFAULT_VISIBLE_CARDS_COUNT (4)
+#define MAXIMUM_VISIBLE_CARDS_COUNT (32)
 
 
-@interface ASDecisionCardsView () <ASDecisionCardProtocol>
+@interface ASDecisionCardsView ()
 
+{
+    UIView *animatedView;
+}
 
+@property (strong, nonatomic) NSMutableArray *cardsQueue;
+
+@property (strong, nonatomic, readonly) UIView *topCardInCardsQueue;
+
+@property (assign, nonatomic, readonly) BOOL moreThanVisibles;
 
 @end
 
 
 @implementation ASDecisionCardsView
 
+#pragma mark - Overriding methods
 
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+#pragma mark - Private methods
+
+- (void)setup {
+    _cardsQueue = [NSMutableArray new];
+    self.visibleCardsCount = DEFAULT_VISIBLE_CARDS_COUNT;
+}
+
+- (UIView *)cardFromQueueAtIndex:(NSUInteger)index {
+    if (_cardsQueue.count == 0 || index >= _cardsQueue.count) return nil;
+    return _cardsQueue[index];
+}
+
+- (void)addCardToSuperview:(UIView *)card {
+    if (self.subviews.lastObject) {
+        [self insertSubview:card belowSubview:self.subviews.firstObject];
+    } else {
+        [self addSubview:card];
+    }
+}
+
+- (void)addCardFromCardsQueue {
+    if (self.moreThanVisibles || !self.topCardInCardsQueue) return;
+    UIView *card = self.topCardInCardsQueue;
+    [_cardsQueue removeObject:card];
+    [self addCardToSuperview:card];
+    [self startAnimating];
+}
+
+- (void)startAnimating {
+    UIView *card = self.subviews.firstObject;
+    animatedView = card;
+    if ([_delegate respondsToSelector:@selector(decisionCardsView:cardBeforeMovingToVisibleQueue:)]) {
+        [_delegate decisionCardsView:self cardBeforeMovingToVisibleQueue:card];
+    }
+    if ([_delegate respondsToSelector:@selector(decisionCardsView:cardAfterMovingToVisibleQueue:)]) {
+        [UIView animateWithDuration:.64f delay:0.f options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            [self->_delegate decisionCardsView:self cardAfterMovingToVisibleQueue:card];
+        } completion:^(BOOL finished) {
+            self->animatedView = nil;
+        }];
+    }
+}
+
+#pragma mark - Public methods
+
+- (void)addCard:(UIView *)card {
+    if (!card) @throw [NSException exceptionWithName:@"ASInvalidParameterException" reason:@"View is can not be a nil" userInfo:nil];
+    if (self.moreThanVisibles) {
+        [_cardsQueue addObject:card];
+    } else {
+        [self addCardToSuperview:card];
+        [self startAnimating];
+    }
+}
+
+- (void)removeCardsAnimated:(BOOL)animated {
+#warning Implement.
+}
+
+- (void)madeDecisionWithType:(ASDecisionType)type {
+    if (self.topCard == animatedView) return;
+    [self.topCard removeFromSuperview];
+    [self addCardFromCardsQueue];
+}
+
+#pragma mark - Private properties
+
+- (UIView *)topCardInCardsQueue {
+    return [self cardFromQueueAtIndex:0];
+}
+
+- (BOOL)moreThanVisibles {
+    return self.subviews.count >= _visibleCardsCount;
+}
+
+#pragma mark - Public properties
+
+- (void)setVisibleCardsCount:(NSUInteger)visibleCardsCount {
+    NSUInteger maximum = MAXIMUM_VISIBLE_CARDS_COUNT;
+    _visibleCardsCount = visibleCardsCount == 0 ? 1 : (visibleCardsCount > maximum ? maximum : visibleCardsCount);
+}
+
+- (NSUInteger)cardsCount {
+    return self.subviews.count + _cardsQueue.count;
+}
+
+- (UIView *)topCard {
+    return self.subviews.lastObject;
+}
 
 @end
